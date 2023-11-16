9039812c11285e540d3a17ffcd752ddc76675242
Kemal Acar <kemallacar@gmail.com>
2023-11-16 23:00:00 +0100

    configurations added

---
+++ backend/src/main/java/com/aid/api/configuration/HttpLogger.java
@@ -0,0 +1,54 @@
+package com.aid.api.configuration;
+
+import com.aid.api.log.LogDto;
+import com.aid.api.log.LogThreadLocal;
+import com.aid.api.util.Constants;
+import lombok.extern.slf4j.Slf4j;
+import net.bytebuddy.utility.RandomString;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+
+@Component
+@Slf4j
+public class HttpLogger extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        boolean dontLog = request.getServletPath().equalsIgnoreCase("/")
+                || request.getServletPath().contains("swagger")
+                || request.getServletPath().contains("actuator")
+                || request.getServletPath()
+                .equalsIgnoreCase("/v2/api-docs");
+
+        if (dontLog) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        if ("OPTIONS".equals(request.getMethod())) {
+            response.setStatus(HttpServletResponse.SC_OK);
+        } else {
+
+            try {
+                LogThreadLocal.set(LogDto.builder().startTime(System.currentTimeMillis()).txId(RandomString.make()).build());
+                filterChain.doFilter(request, response);
+                LogDto logDto = LogThreadLocal.get();
+                log.info("path = [{}], txId=[{}] response time = [{}] ms ",
+                        request.getRequestURI().replace(Constants.API_REQUEST_MAPPING, ""), logDto.getTxId(), System.currentTimeMillis() - logDto.getStartTime());
+            } finally {
+                LogThreadLocal.unset();
+            }
+
+
+        }
+    }
+
+}
+
---
+++ backend/src/main/java/com/aid/api/configuration/SwaggerConfiguration.java
@@ -0,0 +1,49 @@
+package com.aid.api.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import springfox.documentation.builders.AuthorizationScopeBuilder;
+import springfox.documentation.service.*;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+import static com.google.common.collect.Lists.newArrayList;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfiguration extends WebMvcConfigurationSupport {
+
+	@Bean
+	public Docket appApi() {
+		AuthorizationScope[] authScopes = new AuthorizationScope[1];
+		authScopes[0] = new AuthorizationScopeBuilder().scope("global").description("full access").build();
+		SecurityReference securityReference = SecurityReference.builder().reference("Authorization")
+				.scopes(authScopes).build();
+
+		ArrayList<SecurityContext> securityContexts = newArrayList(
+				SecurityContext.builder().securityReferences(newArrayList(securityReference)).build());
+		return new Docket(DocumentationType.SWAGGER_2)
+				.securitySchemes(newArrayList(new ApiKey("Authorization", "Authorization", "header")))
+				.securityContexts(securityContexts).apiInfo(apiInfo()).select().build();
+	}
+
+	private ApiInfo apiInfo() {
+		return new ApiInfo("SPRING BOOT DEMO", "REST API", "1.0.0", "https://www.my-company.com",
+				new Contact("IT Team", "https://www.my-company.com", "xx@xx.xom"), "TECH",
+				"https://www.my-company.com", Collections.emptyList());
+	}
+
+	@Override
+	protected void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
+
+		registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+	}
+}
